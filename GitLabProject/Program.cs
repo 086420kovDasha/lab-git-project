44d6a354bf8fb82044fde3abf6607c598488ea4b
Daria Kovalenko <kovalenko.daria1125@vu.cdu.edu.ua>
2025-11-10 00:32:54 +0200

    Initial commit: додано базовий консольний додаток

---
+++ GitLabProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GitLabProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Git!");
+            Console.WriteLine("Laboratory work on the version control system");
+            Console.ReadKey();
+        }
+    }
+}
